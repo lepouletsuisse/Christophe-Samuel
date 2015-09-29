f93afffe29d8597bbe42a1ab48fdda91ca469ab2
lepouletsuisse <xxpoulletxx@gmail.com>
2015-09-29 13:42:50 +0200

    Creating POO\Labo02
    Creating project

---
+++ POO/Labo02/src/Main.java
@@ -0,0 +1,20 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 02
+ Fichier     : Main.java
+ Auteur(s)   : Samuel Darcey & Christophe Peretti
+ Date        : 29.09.2015
+
+ But         : Trouver la relation f() entre L et la taille N d'un problème de grille
+
+ Remarque(s) : Travail à 2
+
+ Compilateur : jdk1.8.0_60
+ -----------------------------------------------------------------------------------
+*/
+
+public class Main {
+    public static void main(String args[]) {
+
+    }
+}
